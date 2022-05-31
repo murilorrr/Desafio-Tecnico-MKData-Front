3d4de2acebeaefbfe5fbaaba547d34d5e1bf778c
Murilo Ribeiro <murilorsv14@gmail.com>
2022-05-31 14:30:27 -0300

    feat: adiciona estilo para a parte de criação de clientes

--- src/components/CustomersCreateForm.jsx
+++ src/components/CustomersCreateForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { getAllGroups, createCustomer } from "../fetches";
 
 function CustomersCreateForm() {
@@ -72,7 +72,7 @@ function CustomersCreateForm() {
   return(
     <div>
       <h1>CRIAR CLIENTE</h1>
-      <form onSubmit={ handleSubmit }>
+      <FormCreateAnyUser onSubmit={ handleSubmit }>
         <input
           type="text"
           placeholder="Nome"
@@ -136,70 +136,71 @@ function CustomersCreateForm() {
         >
           <div>{warning}</div>
         </div>
-      </form>
+      </FormCreateAnyUser>
     </div>
   );
 }
 
-// const FormCreateAnyUser = styled.form`
-//   margin: 0.5rem 0 2rem;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   border-bottom: 1px dashed var(--gray-200);
+const FormCreateAnyUser = styled.form`
+  margin: 0.5rem 0 2rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  border-bottom: 1px dashed var(--gray-200);
 
-//   input {
-//     display: block;
-//     width: -webkit-fill-available;
-//     box-shadow: 0 0 15px rgb(0,0,0,0.05);
-//     font-size: 18px;
-//     border: 1px solid var(--gray-100);
-//     border-radius: 2rem;
-//     padding: 1rem;
-//     margin: 0.5em 0;
-//     color: var(--gray-500);
-//     font-weight: 400;
-//     /* height: 4rem; */
+  input {
+    display: block;
+    width: 60%;
+    box-shadow: 0 0 15px rgb(0,0,0,0.05);
+    font-size: 18px;
+    border: 1px solid var(--gray-100);
+    border-radius: 2rem;
+    padding: 1rem;
+    margin: 0.5em 0;
+    color: var(--gray-500);
+    font-weight: 400;
+    /* height: 4rem; */
 
-//   }
+  }
 
-//   select {
-//     /* background: var(--crimson); */
-//     /* border: 2px solid var(--crimson); */
-//     border-radius: 2rem;
-//     width: 100%;
-//     padding: 1rem;
-//     font-size: 18px;
-//     margin: 0.5rem;
-//     background: var(--gray-50);
-//     color: var(--gray-500);
-//     box-shadow: 0 0 15px rgb(0,0,0,0.05);
-//     border: none;
-//     font-weight: 400;
-//     /* height: 4rem; */
+  select {
+    /* background: var(--crimson); */
+    /* border: 2px solid var(--crimson); */
+    border-radius: 2rem;
+    width: 30%;
+    padding: 1rem;
+    font-size: 18px;
+    margin: 0.5rem;
+    background: var(--gray-50);
+    color: var(--gray-500);
+    box-shadow: 0 0 15px rgb(0,0,0,0.05);
+    border: none;
+    font-weight: 400;
+    /* height: 4rem; */
 
-//     option {
-//       color: var(--gray-500);
-//     }
-//   }
+    option {
+      color: var(--gray-500);
+    }
+  }
 
-//   button {
-//     background: var(--crimson);
-//     font-weight: 600;
-//     /* width: 5.5rem; */
-//     width: 100%;
-//     /* height: 2rem; */
-//     border-radius: 2rem;
-//     color: var(--gray-50);
-//     margin: 0.5rem 0 1.2rem;
-//     padding: 1rem;
-//     font-size: 0.90rem;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     flex-direction: column;
-//     box-shadow: 0 0 15px rgb(0,0,0,0.05);
-//   }
-// `;
+  button {
+    background: var(--crimson);
+    font-weight: 600;
+    /* width: 5.5rem; */
+    width: 100%;
+    /* height: 2rem; */
+    border-radius: 2rem;
+    color: var(--gray-50);
+    margin: 0.5rem 0 1.2rem;
+    padding: 1rem;
+    font-size: 0.90rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: 0 0 15px rgb(0,0,0,0.05);
+  }
+`;
 
 export default CustomersCreateForm;
