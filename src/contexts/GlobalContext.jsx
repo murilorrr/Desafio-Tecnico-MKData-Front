4c121fbe5671ea483b3afb5c19f601db81fc8245
Murilo Ribeiro <murilorsv14@gmail.com>
2022-06-01 12:13:25 -0300

    feat: adiciona a responsabilidade de guardar os customers e groups para o context

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -5,10 +5,16 @@ export const GlobalContext = createContext();
 
 function GlobalProvider({ children }) {
   const [warning, setWarning] = useState('');
+  const [groups, setGroups] = useState([]);
+  const [customers, setCustomers] = useState([]);
 
   const context = {
     warning,
     setWarning,
+    groups,
+    setGroups,
+    customers,
+    setCustomers,
   };
 
   const contextMemo = useMemo(() => context, [context]);
